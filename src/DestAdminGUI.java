fabb8d259c4a62d977357ea980e126a19a4651f6
dai19024@uom.edu.gr <dai19024@uom.edu.gr>
2020-06-09 16:29:02 +0300

    Προσθηκη DestAdminGUI, GroupTrip

---
+++ src/DestAdminGUI.java
@@ -0,0 +1,384 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.util.ArrayList;
+
+import javax.swing.*;
+import java.awt.Color;
+import java.awt.Font;
+
+public class DestAdminGUI extends JFrame{
+	
+	private JPanel panel = new JPanel();
+	private JList actiList ;
+	private JList list = new JList();
+	private ArrayList<Destination> dests;
+	private ArrayList<String> acts;
+	private JButton create = new JButton("Create");
+	private JButton update = new JButton("Update");
+	private JButton delete = new JButton("Delete");
+	private JButton view = new JButton("View");
+	private JButton back = new JButton("Back");
+	private Destination selectedDest; 
+	
+	public DestAdminGUI() {
+		panel.setBackground(new Color(0, 153, 153));
+		this.setContentPane(panel);
+		try {
+			FileInputStream fileIn = new FileInputStream("destination.ser");
+			ObjectInputStream in = new ObjectInputStream(fileIn);
+			dests = (ArrayList<Destination>) in.readObject();
+			in.close();
+			fileIn.close();		
+		}
+		catch(IOException i) {
+			i.printStackTrace();
+		}
+		catch(ClassNotFoundException c) {
+			c.printStackTrace();
+		}
+		finally {
+			System.out.println("De-Serialization Attempted...");
+		
+		}
+		
+		try {
+			FileInputStream fileIn = new FileInputStream("activities.ser");
+			ObjectInputStream in = new ObjectInputStream(fileIn);
+			acts = (ArrayList<String>) in.readObject();
+			in.close();
+			fileIn.close();		
+		}
+		catch(IOException i) {
+			i.printStackTrace();
+		}
+		catch(ClassNotFoundException c) {
+			c.printStackTrace();
+		}
+		finally {
+			System.out.println("De-Serialization Attempted...");
+		
+		}
+		
+		
+		this.setResizable(false);
+		this.setSize(440,304);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setVisible(true);
+		this.setTitle("Destinations");
+		this.setLocationRelativeTo(null);
+		
+		DefaultListModel model = new DefaultListModel();
+		
+		for(Destination item:dests) {
+			model.addElement(item.getName());
+		}
+		panel.setLayout(null);
+		list.setBounds(18, 40, 392, 157);
+		
+		list.setModel(model);
+		
+		getContentPane().add(list);
+		view.setBounds(18, 217, 65, 23);
+		
+		getContentPane().add(view);
+		create.setBounds(93, 217, 72, 23);
+		getContentPane().add(create);
+		update.setBounds(175, 217, 78, 23);
+		getContentPane().add(update);
+		delete.setBounds(263, 217, 72, 23);
+		getContentPane().add(delete);
+		back.setBounds(345, 217, 65, 23);
+		getContentPane().add(back);
+		
+		JLabel destinationsLabel = new JLabel("Manage Destinations");
+		destinationsLabel.setForeground(Color.WHITE);
+		destinationsLabel.setFont(new Font("Tahoma", Font.BOLD, 16));
+		destinationsLabel.setBounds(18, 11, 201, 18);
+		panel.add(destinationsLabel);
+		ButtonListener listener = new ButtonListener();
+		
+		view.addActionListener(listener);
+		create.addActionListener(listener);
+		update.addActionListener(listener);
+		delete.addActionListener(listener);
+		back.addActionListener(listener);
+		
+	}
+
+	class ButtonListener implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if(e.getSource().equals(create)) {
+				
+				JFrame createFrame;
+				JPanel createPanel = new JPanel();
+				
+				JLabel name = new JLabel("Name");
+				
+				JLabel activities = new JLabel("Choose activities");
+				
+				actiList = new JList(acts.toArray());
+				actiList.setSelectionModel(new DefaultListSelectionModel() {
+				    @Override
+				    public void setSelectionInterval(int index0, int index1) {
+				        if(super.isSelectedIndex(index0)) {
+				            super.removeSelectionInterval(index0, index1);
+				        }
+				        else {
+				            super.addSelectionInterval(index0, index1);
+				        }
+				    }
+				});
+				
+				
+				JTextField nameField = new JTextField(10);
+				
+				JButton save = new JButton("Save");
+				JButton cancel = new JButton("Cancel");
+				
+				createPanel.add(name);
+				createPanel.add(nameField);
+				createPanel.add(activities);
+				createPanel.add(actiList);
+				
+				createPanel.add(save);
+				createPanel.add(cancel);
+				
+				hide();
+				createFrame = new JFrame();
+				createFrame.setContentPane(createPanel);
+			
+				createFrame.setResizable(false);
+				createFrame.setSize(200,250);
+				createFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+				createFrame.setVisible(true);
+				createFrame.setTitle("Tour-X Create Destination");
+				createFrame.setLocationRelativeTo(null);
+				
+				save.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+							
+						
+						int[] selIndices= actiList.getSelectedIndices();
+						ArrayList<String> actArray = new ArrayList<String>();
+						for (int i = 0; i < selIndices.length; i++)
+						{
+						      actArray.add( String.valueOf(actiList.getModel().getElementAt(selIndices[i])));
+					    }
+						
+					
+							Destination d = new Destination(nameField.getText(),actArray);
+							dests.add(d);
+							
+							try {
+								FileOutputStream fileOutTickets = new FileOutputStream("destination.ser");
+								ObjectOutputStream out = new ObjectOutputStream(fileOutTickets);
+								out.writeObject(dests);
+								out.close();
+								fileOutTickets.close();		
+							}
+							catch(IOException i) {
+								i.printStackTrace();
+							}
+							finally {
+								System.out.println("Serialization Attempted Destinations...");
+							}
+							
+							createFrame.hide();
+							DestAdminGUI GUIA = new DestAdminGUI();
+					}
+				});
+		
+				cancel.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+							
+							createFrame.hide();
+							DestAdminGUI GUIA = new DestAdminGUI();
+					}
+				});
+				}
+			else 
+			{
+				
+				String selectedDestName =(String) list.getSelectedValue();
+				
+				selectedDest = null;
+				for(Destination d: dests) 									
+					if(d.getName().equals(selectedDestName))
+						selectedDest = d;
+				
+				JFrame createFrame;
+				JPanel createPanel = new JPanel();
+				JTextArea actArea ;
+				
+				if (e.getSource().equals(view)) 			//VIEW
+				{
+					
+				JLabel name = new JLabel("Name: " + selectedDest.getName());
+				JLabel actLabel = new JLabel ( "            Activities");
+				
+				String ex2 = "";
+				for (String a: selectedDest.getActivities())
+					ex2 += a + "  " ;
+				   
+				
+				actArea = new JTextArea(ex2);
+				actArea.setLineWrap(true);
+				actArea.setWrapStyleWord(true);
+				actArea.setEditable(false);
+				
+				JButton back = new JButton("Back");
+				createPanel.add(name);
+				createPanel.add(actLabel);
+				createPanel.add(actArea);
+				createPanel.add(back);
+				
+				hide();
+				createFrame = new JFrame();
+				createFrame.setContentPane(createPanel);
+			
+				createFrame.setResizable(false);
+				createFrame.setSize(200,250);
+				createFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+				createFrame.setVisible(true);
+				createFrame.setTitle("Tour-X View Destination");
+				createFrame.setLocationRelativeTo(null);
+				
+				back.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+							
+							createFrame.hide();
+							DestAdminGUI GUIA = new DestAdminGUI();
+					}
+				});
+				
+			}
+			
+			else if (e.getSource().equals(update))					//UPDATE
+			{
+				actiList = new JList(acts.toArray());
+				actiList.setSelectionModel(new DefaultListSelectionModel() {
+				    @Override
+				    public void setSelectionInterval(int index0, int index1) {
+				        if(super.isSelectedIndex(index0)) {
+				            super.removeSelectionInterval(index0, index1);
+				        }
+				        else {
+				            super.addSelectionInterval(index0, index1);
+				        }
+				    }
+				});
+				
+		        
+				JLabel name = new JLabel("Name");
+				JLabel activities = new JLabel("Choose new activities");
+				
+				
+				JTextField nameField = new JTextField(10);
+
+				JButton update = new JButton("Update");
+				JButton cancel = new JButton("Cancel");
+				
+				createPanel.add(name);
+				createPanel.add(nameField);
+				createPanel.add(activities);
+				createPanel.add(actiList);
+				
+				createPanel.add(update);
+				createPanel.add(cancel);
+				
+				hide();
+				createFrame = new JFrame();
+				createFrame.setContentPane(createPanel);
+			
+				createFrame.setResizable(false);
+				createFrame.setSize(200,250);
+				createFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+				createFrame.setVisible(true);
+				createFrame.setTitle("Tour-X Update Destination");
+				createFrame.setLocationRelativeTo(null);
+				
+				update.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						createFrame.hide();
+						if (!nameField.getText().isEmpty())
+							selectedDest.setName(nameField.getText());
+						
+						int[] selIndices= actiList.getSelectedIndices();
+						ArrayList<String> actArray = new ArrayList<String>();
+						for (int i = 0; i < selIndices.length; i++)
+						{
+						      actArray.add( String.valueOf(actiList.getModel().getElementAt(selIndices[i])));
+					    }
+						
+						if (selIndices.length!=0)
+							selectedDest.setActivities(actArray);
+							
+							try {
+								FileOutputStream fileOutTickets = new FileOutputStream("destination.ser");
+								ObjectOutputStream out = new ObjectOutputStream(fileOutTickets);
+								out.writeObject(dests);
+								out.close();
+								fileOutTickets.close();		
+							}
+							catch(IOException i) {
+								i.printStackTrace();
+							}
+							finally {
+								System.out.println("Serialization Attempted Destinations...");
+							}
+						
+							createFrame.hide();
+							DestAdminGUI GUIA = new DestAdminGUI();
+				
+					}
+				});
+		
+				cancel.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+							
+							createFrame.hide();
+							DestAdminGUI GUIA = new DestAdminGUI();
+					}
+				});
+				
+			}
+			
+			else if (e.getSource().equals(delete))													//DELETE
+			{   hide();
+				for (Destination d:dests)
+				{
+					if ( d.getName().equals(selectedDest.getName()))
+						{dests.remove(d);
+						
+						try {
+							FileOutputStream fileOutTickets = new FileOutputStream("destination.ser");
+							ObjectOutputStream out = new ObjectOutputStream(fileOutTickets);
+							out.writeObject(dests);
+							out.close();
+							fileOutTickets.close();		
+						}
+						catch(IOException i) {
+							i.printStackTrace();
+						}
+						finally {
+							System.out.println("Serialization Attempted Destinations...");
+						
+						}
+					
+					      hide();
+					      break;}
+					
+				}
+				DestAdminGUI GUIA = new DestAdminGUI();
+			}
+			else
+			{
+				hide();
+				new AdminHomepageGUI();
+			}
+		}}
+	}
+}
---
+++ src/GroupTrip.java
@@ -0,0 +1,124 @@
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class GroupTrip extends TripItem{
+
+	private Destination destination;
+	private Hotel hotel ; 
+	private Tickets ticket;
+	private ArrayList <String> choices;
+	private ArrayList <Traveller> travellers;
+	private int capacity;
+	private ArrayList<String> activities;
+	
+	public GroupTrip()
+	{
+		super("",0);
+		travellers = new ArrayList<Traveller>();
+	}
+	
+	
+	public GroupTrip(String name, double price, int capacity, Destination destination, Hotel hotel, Tickets ticket,
+									ArrayList<Traveller> travellers ) { 
+		super(name,price);
+		this.destination = destination;
+		this.hotel = hotel;
+		this.ticket = ticket;
+		this.choices = choices;
+		this.travellers = travellers;
+		this.capacity = capacity;
+		this.activities = destination.getActivities();
+	}
+	
+	public boolean TravellerExists (Traveller traveller)
+	{
+		boolean found= false;
+		for (Traveller t: travellers)
+			if (t.username.equals(traveller.username))
+				found=true;
+				
+		return found;
+	}
+	
+	public void addTraveller(Traveller traveller)
+	{
+		if(travellers.size()<capacity)
+			travellers.add(traveller);
+		
+		else
+		{
+			JFrame Full = new JFrame();
+			JOptionPane.showMessageDialog(Full, "This Group is full");
+		}
+	}
+	
+	public boolean isFull()
+	{
+		if(capacity == travellers.size())
+			return true;
+		return false;
+	}
+	
+	public Destination getDestination() {
+		return destination;
+	}
+
+	public void setDestination(Destination destination) {
+		this.destination = destination;
+	}
+
+	public Hotel getHotel() {
+		return hotel;
+	}
+
+	public void setHotel(Hotel hotel) {
+		this.hotel = hotel;
+	}
+
+	public Tickets getTicket() {
+		return ticket;
+	}
+
+	public void setTicket(Tickets ticket) {
+		this.ticket = ticket;
+	}
+
+	public ArrayList<String> getChoices() {
+		return choices;
+	}
+
+	public void setChoices(ArrayList<String> choices) {
+		this.choices = choices;
+	}
+
+	public ArrayList<Traveller> getTravellers() {
+		return travellers;
+	}
+
+	public void setTraveller(ArrayList<Traveller> travellers) {
+		this.travellers = travellers;
+	}
+
+	public int getCapacity() {
+		return capacity;
+	}
+
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+
+	public ArrayList<String> getActivities() {
+		return activities;
+	}
+
+	public void setActivities(ArrayList<String> activities) {
+		this.activities = activities;
+	}
+
+	public void setTravellers(ArrayList<Traveller> travellers) {
+		this.travellers = travellers;
+	}
+	
+}
